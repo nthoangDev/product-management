6ec6799d21aadcee3df9c27f248f595958144ef6
nguyen thanh hoang <nthoang0708@gmail.com>
2025-01-02 18:14:46 +0700

    Nhúng bootstrap và static file

--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -1,5 +1,7 @@
 
 //Export ra và đặt tên hàm export là index
 module.exports.index = (req, res) => {
-    res.render("client/pages/home/index")
+    res.render("client/pages/home/index", {
+        pageTitle: "Trang chủ"
+    })
 }
