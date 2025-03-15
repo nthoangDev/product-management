295f185843237a274cab6b26bd27982d178202d6
nguyenthanhoang <nthoang0708@gmail.com>
2025-03-16 00:03:58 +0700

    Làm danh sách sản phẩm bên client

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,5 +1,20 @@
-module.exports.index = (req, res) => {
+Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+
+    const newProducts = products.map(item =>{
+        item.newPrice = (item.price * (100 - item.discountPercentage) / 100).toFixed(0);
+        return item;
+    })
+    
+    console.log(newProducts);
+
     res.render('client/pages/products/index', {
-        pageTitle: "Danh sách sản phẩm"
+        pageTitle: "Danh sách sản phẩm",
+        products: newProducts
     })
 }
