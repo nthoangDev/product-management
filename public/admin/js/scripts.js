7ca008a18b2ae76dcc5cdd0542d23574f3baa2e5
nguyenthanhoang <nthoang0708@gmail.com>
2025-07-16 00:13:53 +0700

    Tính năng lọc theo trạng thái

---
+++ public/admin/js/scripts.js
@@ -0,0 +1,18 @@
+const btnStatus = document.querySelectorAll("[button-status]");
+
+if (btnStatus.length > 0) {
+    btnStatus.forEach(btn => {
+        btn.addEventListener('click', () => {
+            let url = new URL(window.location.href);
+
+            let status = btn.getAttribute("button-status");
+            // console.log(status);
+            if (status) {
+                url.searchParams.set("status", status);
+            }else{
+                url.searchParams.delete("status");
+            }
+            window.location.href = url.href;
+        })
+    });
+}
